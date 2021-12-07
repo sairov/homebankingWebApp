7e78f8e193e97e600d0be05efadbb7c725f0a1c2
SairovARG <pmrovira92@gmail.com>
2021-12-07 20:19:31 +0000

    [ADD] first commit with inital data

---
+++ src/main/java/config/DBConn.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author pol_m
+ */
+public class DBConn {
+    
+    private String host = "jdbc:mysql://localhost:3306/";
+    
+    public Connection getConnection(String database, String userMysql, String passMysql ) {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(host + database, userMysql, passMysql);
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
